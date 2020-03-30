d56939c0edbf348df8c13e9219b291efde5b741e
jinio <jinioh88@gmail.com>
2020-03-31 08:42:58 +0900

    람다식으로 변경

--- src/ch09/Main.java
+++ src/ch09/Main.java
@@ -1,15 +1,10 @@
 package ch09;
 
-import ch09.strategy.IsAllLowerCase;
-import ch09.strategy.IsNumeric;
 import ch09.strategy.Validator;
 
 public class Main {
     public static void main(String[] args) {
-        Validator validator1 = new Validator(new IsNumeric());
-        boolean b1 = validator1.validate("aaaa");
-
-        Validator validator2 = new Validator(new IsAllLowerCase());
-        boolean b2 = validator2.validate("bbbb");
+        Validator numbericValidator = new Validator((String s) -> s.matches("[a-z]+"));
+        boolean b1 = numbericValidator.validate("aaaa");
     }
 }
